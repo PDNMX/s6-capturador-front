b9b301f0bbc25f68b6c7a75dec94266b71ecef87
Benito Perdomo Vergara <44103502+RazielSystems@users.noreply.github.com>
2024-06-27 16:48:42 -0600

    Revert "Dev benito"

--- src/app/auth/tender/tender.component.ts
+++ src/app/auth/tender/tender.component.ts
@@ -1,74 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tender',
   templateUrl: './tender.component.html',
-  styleUrls: ['./tender.component.css'],
+  styleUrls: ['./tender.component.css']
 })
-export class TenderComponent implements OnInit {
-  tenderForm!: FormGroup;
+export class TenderComponent {
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
-    this.tenderForm = this.fb.group({
-      title: [''],
-      additionalProcurementCategories: [''],
-      awardCriteria: [''],
-      awardCriteriaDetails: [''],
-      awardPeriod: this.fb.group({
-        durationInDays: [''],
-        endDate: [''],
-        maxExtentDate: [''],
-        startDate: [''],
-      }),
-      contractPeriod: this.fb.group({
-        durationInDays: [''],
-        endDate: [''],
-        maxExtentDate: [''],
-        startDate: [''],
-      }),
-      description: [''],
-      eligibilityCriteria: [''],
-      enquiryPeriod: this.fb.group({
-        durationInDays: [''],
-        endDate: [''],
-        maxExtentDate: [''],
-        startDate: [''],
-      }),
-      hasEnquiries: [''],
-      id: [''],
-      mainProcurementCategory: [''],
-      minValue: this.fb.group({
-        amount: [''],
-        currency: [''],
-      }),
-      numberOfTenderers: [''],
-      procurementMethod: [''],
-      procurementMethodDetails: [''],
-      procurementMethodRationale: [''],
-      procuringEntity: this.fb.group({
-        id: [''],
-        name: [''],
-      }),
-      status: [''],
-      submissionMethod: [''],
-      submissionMethodDetails: [''],
-      tenderPeriod: this.fb.group({
-        durationInDays: [''],
-        endDate: [''],
-        maxExtentDate: [''],
-        startDate: [''],
-      }),
-      value: this.fb.group({
-        amount: [''],
-        currency: [''],
-      }),
-    });
-  }
-
-  onSubmit(): void {
-    console.log(this.tenderForm.value);
-  }
 }
