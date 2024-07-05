eb66c000ae8c4a4239b972241e04ca496d799419
Andres Lopez <alopezr2129@gmail.com>
2024-07-04 19:24:14 -0600

    avance estructura del JSON form awards

--- src/app/auth/awards/awards.component.ts
+++ src/app/auth/awards/awards.component.ts
@@ -4,36 +4,78 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-awards',
   templateUrl: './awards.component.html',
-  styleUrls: ['./awards.component.css']
+  styleUrls: ['./awards.component.css'],
 })
 export class AwardsComponent implements OnInit {
-  
-  constructor(private fb: FormBuilder) { }
-  ngOnInit() { }
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {}
 
-    Awards = this.fb.group({
+  Awards = this.fb.group({
+    id: ['', Validators.required],
+    status: ['', Validators.required],
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    rationale: ['', Validators.required],
+    date: ['', Validators.required],
+    value: this.fb.group({
+      amount: ['', Validators.required],
+      currency: ['', Validators.required],
+    }),
+    contractPeriod: this.fb.group({
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      maxExtentDate: ['', Validators.required],
+      durationInDays: ['', Validators.required],
+    }),
+  });
+
+  AwardsSuppliers = this.fb.group({
+    suppliers: this.fb.group({
+      id: [''],
+      name: [''],
+    }),
+  });
+
+  AwardsItems = this.fb.group({
+    items: this.fb.group({
       id: ['', Validators.required],
-      status: ['', Validators.required],
-      title: ['', Validators.required],
       description: ['', Validators.required],
-      rationale: ['', Validators.required],
-      date: ['', Validators.required],
+      quantity: ['', Validators.required],
+    }),
+    unit: this.fb.group({
+      id: [''],
+      scheme: [''],
+      name: [''],
+      uri: [''],
       value: this.fb.group({
-        amount: ['', Validators.required],
-        currency: ['', Validators.required],
+        amount: [''],
+        currency: [''],
       }),
-      contractPeriod: this.fb.group({
-        startDate: ['', Validators.required],
-        endDate: ['', Validators.required],
-        maxExtentDate: ['', Validators.required],
-        durationInDays: ['', Validators.required]
-      })
-    });
+    }),
+    
+    classification: this.fb.group({
+      id: ['', Validators.required],
+      description: ['', Validators.required],
+      scheme: [''],
+      uri: [''],
+    }),
+    additionalClassifications: this.fb.group({
+      id: ['', Validators.required],
+      description: ['', Validators.required],
+      scheme: [''],
+      uri: [''],
+    }),
+  });
 
-    AwardsSuppliers = this.fb.group({
-      
-    })
-  onSubmit(): void {
+  onSubmit() {
     console.log(this.Awards.value);
+  }
+
+  onSubmitSuppliers() {
+    console.log(this.AwardsSuppliers.value);
+  }
+
+  onSubmitItems() {
+    console.log(this.AwardsItems.value);
   }
 }
