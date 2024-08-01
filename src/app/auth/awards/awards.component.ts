c3ff01fbaf3db177804876700c74b2b7e28b5ce7
Andres Lopez <alopezr2129@gmail.com>
2024-07-31 19:41:41 -0600

    Guadando las secciones generales y suppliers

--- src/app/auth/awards/awards.component.ts
+++ src/app/auth/awards/awards.component.ts
@@ -10,10 +10,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AwardsComponent implements OnInit {
   data: any;
   data1: any;
+  savingMessage: string = '';
+  isSaving: boolean = false;
   /* Datos de ejemplo */
-  dataToSend = {};
+  //dataToSend = {};
   /*   id: string = '1';
   dataToUpdate = {}; */
+
+  //Almacenar los datos temporalmente para cada sección
+  tempAwards: any = {
+  suppliers: [],
+  }
 
   constructor(private fb: FormBuilder, private apiService: ApiService) {}
   ngOnInit(): void {
@@ -22,7 +29,7 @@ export class AwardsComponent implements OnInit {
 
   //Para ser usado con el api del s6
   postMethod(dataToSend: any) {
-    this.apiService.postMethod<any>(dataToSend, '/awards/insert').subscribe(
+    this.apiService.postMethod<any>(dataToSend, 'awards/insert').subscribe(
       (data1: any) => {
         console.log('Data returned successfully:', data1);
         this.data1 = data1;
@@ -122,27 +129,16 @@ export class AwardsComponent implements OnInit {
 
   onSubmit() {
     console.log(this.awards.value);
-    let dataaward: any = {
-      id: this.awards.value.id,
-      data: {
-        award: this.awards.value,
-      },
-    };
-    console.log('Mandando datos');
-    console.log(dataaward);
-    this.postMethod(dataaward);
+    this.tempAwards = {...this.tempAwards, ...this.awards.value};
+    this.showSavingMessage();
+    this.awards.reset();
   }
 
   onSubmitSuppliers() {
     console.log(this.suppliers.value);
-    let datasupplier: any = {
-      data: {
-        supplier: this.suppliers.value,
-      },
-    };
-    console.log('mandando datos de supplier');
-    console.log(datasupplier);
-    this.postMethod(datasupplier);
+    this.tempAwards.suppliers.push(this.suppliers.value);
+    this.showSavingMessage();
+    this.suppliers.reset();
   }
 
   onSubmitItems() {
@@ -155,5 +151,30 @@ export class AwardsComponent implements OnInit {
 
   onSubmitAwardsAmendments() {
     console.log(this.amendments.value);
+  }
+
+  //Metodo del mensaje guardando
+  showSavingMessage() {
+    this.isSaving = true;
+    this.savingMessage = 'Guardando...';
+    setTimeout(() => {
+      this.isSaving = false;
+      this.savingMessage = '';
+    }, 3000);
+  }
+
+  //Metodo para combinar y enviar todos los datos
+  submitAllSections() {
+    const finalData = {
+      id: this.tempAwards.id,
+      data: {
+        award:{
+          ...this.tempAwards,
+          suppliers:this.tempAwards.suppliers,
+        }
+      }
+    };
+    console.log('Enviando todos los datos', finalData);
+    this.postMethod(finalData);
   }
 }
