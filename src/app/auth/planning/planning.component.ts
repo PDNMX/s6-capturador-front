11d6b04bc4fbb2d998fd10b908c7d5e9460fcadb
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2024-09-11 20:03:09 -0600

    example get id in component

--- src/app/auth/planning/planning.component.ts
+++ src/app/auth/planning/planning.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-planning',
   templateUrl: './planning.component.html',
-  styleUrls: ['./planning.component.css']
+  styleUrls: ['./planning.component.css'],
 })
-export class PlanningComponent {
+export class PlanningComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      console.log('planning id: ', id);
+    });
+  }
 }
