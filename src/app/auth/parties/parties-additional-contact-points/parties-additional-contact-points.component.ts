68f76dc073b3b413719106ff167609a6df8795c2
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2024-09-27 18:04:07 -0600

    add sections parties

---
+++ src/app/auth/parties/parties-additional-contact-points/parties-additional-contact-points.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parties-additional-contact-points',
+  templateUrl: './parties-additional-contact-points.component.html',
+  styleUrls: ['./parties-additional-contact-points.component.css']
+})
+export class PartiesAdditionalContactPointsComponent {
+
+}
---
+++ src/app/auth/parties/parties-beneficial-owners/parties-beneficial-owners.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parties-beneficial-owners',
+  templateUrl: './parties-beneficial-owners.component.html',
+  styleUrls: ['./parties-beneficial-owners.component.css']
+})
+export class PartiesBeneficialOwnersComponent {
+
+}
