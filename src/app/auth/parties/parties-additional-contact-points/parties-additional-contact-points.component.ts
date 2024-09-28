8503d54e5fbba954f4bedac7756042db0349bdb9
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2024-09-27 20:26:09 -0600

    link form with fb

--- src/app/auth/parties/parties-additional-contact-points/parties-additional-contact-points.component.ts
+++ src/app/auth/parties/parties-additional-contact-points/parties-additional-contact-points.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-parties-additional-contact-points',
   templateUrl: './parties-additional-contact-points.component.html',
-  styleUrls: ['./parties-additional-contact-points.component.css']
+  styleUrls: ['./parties-additional-contact-points.component.css'],
 })
-export class PartiesAdditionalContactPointsComponent {
+export class PartiesAdditionalContactPointsComponent implements OnInit {
+  additionalContactPointsForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.additionalContactPointsForm = this.fb.group({
+      name: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+      givenName: ['', [Validators.required]],
+      patronymicName: ['', [Validators.required]],
+      matronymicName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      telephone: ['', [Validators.required]],
+      faxNumber: ['', [Validators.required]],
+      url: ['', [Validators.required]],
+      availableLanguage: ['', [Validators.required]],
+    });
+  }
 }
--- src/app/auth/parties/parties-beneficial-owners/parties-beneficial-owners.component.ts
+++ src/app/auth/parties/parties-beneficial-owners/parties-beneficial-owners.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-parties-beneficial-owners',
   templateUrl: './parties-beneficial-owners.component.html',
-  styleUrls: ['./parties-beneficial-owners.component.css']
+  styleUrls: ['./parties-beneficial-owners.component.css'],
 })
-export class PartiesBeneficialOwnersComponent {
+export class PartiesBeneficialOwnersComponent implements OnInit {
+  beneficialOwnersForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.beneficialOwnersForm = this.fb.group({
+      name: ['', [Validators.required]],
+      identifier: ['', [Validators.required]],
+      nationality: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      telephone: ['', [Validators.required]],
+      faxNumber: ['', [Validators.required]],
+      address: this.fb.group({
+        streetAddress: ['', [Validators.required]],
+        locality: ['', [Validators.required]],
+        region: ['', [Validators.required]],
+        postalCode: ['', [Validators.required]],
+        countryName: ['', [Validators.required]],
+      }),
+    });
+  }
 }
