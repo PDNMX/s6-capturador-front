67b5f03a0a96892766ed70f56af27e85578f86bf
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2024-10-09 17:18:56 -0600

    save commit

--- src/app/auth/planning/planning-requestForQuotes/planning-requestForQuotes.component.ts
+++ src/app/auth/planning/planning-requestForQuotes/planning-requestForQuotes.component.ts
@@ -1,9 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { ApiService } from 'src/app/services/api.service';
-
-import { Currency } from 'src/utils';
+import { ApiService, IPartieList } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-planning-requestForQuotes',
@@ -11,11 +9,18 @@ import { Currency } from 'src/utils';
   styleUrls: ['./planning-requestForQuotes.component.css'],
 })
 export class PlanningRequestForQuotesComponent implements OnInit {
+  @Input() requestForQuotesArray: Array<any> = [];
+  @Output() addRequestForQuotes = new EventEmitter<any>();
+  @Output() deleteRequestForQuotes = new EventEmitter<any>();
+
   @Output() saveRequestForQuotesData = new EventEmitter<any>();
 
   record_id = null;
 
   requestForQuotesForm!: FormGroup;
+  selectForm!: FormGroup;
+
+  invitedSuppliers: any = [];
 
   constructor(
     private fb: FormBuilder,
@@ -23,21 +28,49 @@ export class PlanningRequestForQuotesComponent implements OnInit {
     private api: ApiService
   ) {}
 
-  setSelectValue(element: string, value: any): void {
-    this.requestForQuotesForm.get(element)?.setValue(value);
+  get quotesArray() {
+    return this.requestForQuotesForm.controls['quotes'] as FormArray;
   }
 
-  loadForm(data: any): void {
-    const { id, title, description, period, items, uri } = data;
+  addQuotes(opt: any) {
+    this.quotesArray.push(opt);
+  }
+  deleteQuotes(index: number) {
+    this.quotesArray.removeAt(index);
+  }
 
-    this.requestForQuotesForm.patchValue({
-      id,
-      title,
-      description,
-      period,
-      items,
-      uri,
-    });
+  get invitedSuppliersArray() {
+    return this.requestForQuotesForm.controls['invitedSuppliers'] as FormArray;
+  }
+
+  addInvitedSuppliers(): void {
+    console.log(
+      'this.selectForm.value.invitedSuppliers: ',
+      this.selectForm.value.invitedSuppliers
+    );
+    this.invitedSuppliersArray.push(
+      this.fb.control(this.selectForm.value.invitedSuppliers)
+    );
+  }
+
+  deleteInvitedSuppliers(index: number): void {
+    this.invitedSuppliersArray.removeAt(index);
+  }
+
+  get itemsArray() {
+    return this.requestForQuotesForm.controls['items'] as FormArray;
+  }
+
+  addItem(opt: any): void {
+    this.itemsArray.push(opt);
+  }
+
+  deleteItem(index: number): void {
+    this.itemsArray.removeAt(index);
+  }
+
+  setSelectValue(element: string, value: any): void {
+    this.requestForQuotesForm.get(element)?.setValue(value);
   }
 
   loadData(): void {
@@ -47,26 +80,40 @@ export class PlanningRequestForQuotesComponent implements OnInit {
 
     this.api.getMethod(`/planning/${this.record_id}`).subscribe((d: any) => {
       const { planning, error, message } = d;
+      console.log('planning: ', planning);
 
       if (error) {
         console.log('message: ', message);
       } else {
         // load forms
-        if (planning !== null) this.loadForm(planning);
+        if (planning.requestForQuotes !== null) {
+          // this.loadForm(planning.requestForQuotes);
+        }
       }
     });
   }
 
   ngOnInit(): void {
     this.initForm();
+
+    this.route.paramMap.subscribe((params: any) => {
+      this.record_id = params.get('id');
+    });
+
+    if (this.record_id) {
+      this.api.getPartiesByType(this.record_id).subscribe((d: IPartieList) => {
+        this.invitedSuppliers = d.data;
+      });
+    }
+
     this.loadData();
   }
 
   initForm(): void {
     this.requestForQuotesForm = this.fb.group({
-      id: ['', Validators.required],
-      title: ['', Validators.required],
-      description: ['', Validators.required],
+      id: ['id', Validators.required],
+      title: ['title', Validators.required],
+      description: ['description', Validators.required],
       period: this.fb.group({
         startDate: ['', Validators.required],
         endDate: ['', Validators.required],
@@ -74,11 +121,23 @@ export class PlanningRequestForQuotesComponent implements OnInit {
         durationInDays: ['', Validators.required],
       }),
       items: this.fb.array([]),
+      invitedSuppliers: this.fb.array([]),
+      quotes: this.fb.array([]),
       uri: ['', Validators.required],
+    });
+
+    this.initSelectForm();
+  }
+
+  initSelectForm(): void {
+    this.selectForm = this.fb.group({
+      invitedSuppliers: ['', [Validators.required]],
     });
   }
 
-  saveForm(): void {
-    this.saveRequestForQuotesData.emit(this.requestForQuotesForm);
+  addNewRequestForQuotes(): void {
+    this.addRequestForQuotes.emit(this.requestForQuotesForm);
+    console.log('this.requestForQuotesForm: ', this.requestForQuotesForm.value);
+    this.initForm();
   }
 }
