a39a9813a37e03bf830c62dc88b2c45ab0df0d53
Andres Lopez <alopezr2129@gmail.com>
2024-11-20 01:09:40 -0600

    Merge branch 'dev' into alopez

---
+++ src/utils/surveillanceMechanismsType.ts
@@ -0,0 +1,28 @@
+const SurveillanceMechanismsType = [
+  {
+    code: 'socialWitness',
+    title: 'Testigo social',
+    description:
+      'Ciudadanos especializados que participan en la supervisión de este proceso de contratación.',
+  },
+  {
+    code: 'citizenComptroller',
+    title: 'Contraloría ciudadana',
+    description:
+      'Mecanismo de vigilancia por parte de los beneficiarios para verificar el avance de las obras y el correcto gasto de los recursos públicos.',
+  },
+  {
+    code: 'internalControlUnit',
+    title: 'Unidad de control interno',
+    description:
+      'Órgano encargado de prevenir, detectar, sancionar y erradicar prácticas corruptas que puedan surgir en este proceso de contratación.',
+  },
+  {
+    code: 'externalAuditor',
+    title: 'Auditor externo',
+    description:
+      'Institución fuera de la administración pública que realiza una auditoría de este proceso de contratación.',
+  },
+];
+
+export default SurveillanceMechanismsType;
