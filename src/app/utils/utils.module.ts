394f0aade42642e3b4e1274e4212f95441c57ce5
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2025-01-15 15:52:04 -0600

    add utils module

---
+++ src/app/utils/utils.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputValidationComponent } from './input-validation/input-validation.component';
+
+@NgModule({
+  declarations: [InputValidationComponent],
+  imports: [CommonModule],
+  exports: [InputValidationComponent],
+})
+export class UtilsModule {}
