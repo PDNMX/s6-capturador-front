28da166f1469c3973be3eeba2fc5d41373feaeed
Benito Perdomo Vergara <benito.perdomo.v@gmail.com>
2025-02-24 19:41:02 -0600

    add management module

---
+++ src/app/management/management.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManagementRoutingModule } from './management-routing.module';
+import { ManagementComponent } from './management.component';
+import { UserManagementModule } from './user-management/user-management.module';
+
+@NgModule({
+  declarations: [ManagementComponent],
+  imports: [CommonModule, ManagementRoutingModule, UserManagementModule],
+})
+export class ManagementModule {}
